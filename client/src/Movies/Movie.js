64506b102e45c71622ae7bf7abe4f781d31ab476
CannaShaq <itscoldinwinter2@gmail.com>
2020-08-09 11:27:00 -0700

    Mvp halfway
    MVP almost complete {home} not working, Movie Cards Not working properly "Only showing star wars movie card" regardless of whats selected.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
@@ -31,6 +32,7 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
+      
       <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   return (
@@ -13,7 +14,9 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore } = movie;
   return (
+    <Link to={`/movies/${movie}`}>
     <div className="movie-card">
+      
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -21,7 +24,9 @@ function MovieDetails({ movie }) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      
     </div>
+    </Link>
   );
 }
 
